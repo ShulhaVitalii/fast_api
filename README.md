896183974247b562fec4270a8361b71594b16fd5
VitaliiAQA <94836456+VitaliiAQA@users.noreply.github.com>
2024-01-04 15:41:41 +0200

    Intro. Path parameters

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fast_api
 Learning Fast API
+
+https://fastapi.tiangolo.com/tutorial/first-steps/
+
+uvicorn main:app --reload
---
+++ main.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/user")
+def get_all_users():
+    return {"users": {}}
+
+
+@app.get("/user/{user_id}")
+def get_all_users(user_id: int):
+    return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
